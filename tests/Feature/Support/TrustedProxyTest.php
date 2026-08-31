9b8eb6b6314f185e906770e1783c34ae193b7f6c
kkz6 <karthickk1996@hotmail.com>
2026-08-31 13:58:38 +0530

    Make proxy test environment independent

--- tests/Feature/Support/TrustedProxyTest.php
+++ tests/Feature/Support/TrustedProxyTest.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 test('local reverse proxies preserve secure storefront URLs', function (): void {
     $this->withServerVariables([
         'REMOTE_ADDR' => '127.0.0.1',
-        'HTTP_HOST' => 'kinkoza.test',
         'HTTP_X_FORWARDED_PROTO' => 'https',
-    ])->get('/')
+    ])->get('http://kinkoza.test/')
         ->assertOk()
         ->assertSee('https://kinkoza.test/cart');
 });
